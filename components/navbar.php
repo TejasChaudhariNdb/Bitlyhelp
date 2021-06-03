cde69b7b290f4fe20d8038c33e325f5818befd40
TejasChaudhariNdb <tejaschaudhari038@gmail.com>
2021-06-03 21:34:22 +0530

    login links

--- components/navbar.php
+++ components/navbar.php
@@ -22,12 +22,12 @@
           
         </li>
         <li class="nav-item">
-          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="/Dashboard/login.php">
             <i class="fa  fa-sign-in text-lg leading-lg text-white opacity-75"></i><span class="ml-2">Login</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="/Dashboard/sign.php">
             <i class="fa  fa-user-circle text-lg leading-lg text-white opacity-75"></i><span class="ml-2">Register</span>
           </a>
         </li>
