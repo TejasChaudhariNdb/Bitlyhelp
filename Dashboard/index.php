7819016fe5078f63e151984e12a6e3856f963466
TejasChaudhariNdb <tejaschaudhari038@gmail.com>
2021-06-04 00:41:16 +0530

    Dashboard ui

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -11,7 +11,122 @@
 <script src="https://use.fontawesome.com/f00636cbad.js"></script>
 
 
+<style>
+
+.box-title{
+display: inline-block;
+font-size: 25px;
+margin: 0;
+line-height: 1;
+
+}
+
+.box-header{
+    color: #444;
+display: block;
+padding: 10px;
+position: relative;
+}
+</style>
+
+
 </head>
-<body>
+<body class="bg-gray-100">
+
+
+
+<?php 
+include("navbar.php");
+include("sidebar.php");
+
+?>
+
+<div class="mt-5 container">
+
+<p class="text-xl text-gray-600"><b>Dashboard</b></p>
+<br>
+
+<div class="bg-white border-b border-gray-200 rounded">
+<ul class="flex p-3 text-gray-500 text-sm lg:text-base">
+                    <li class="inline-flex items-center">
+                      <a href="/">Home</a>
+                      <svg
+                        class="h-5 w-auto text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </li>
+                    <li class="inline-flex items-center">
+                      <a href="/components">Dashboard</a>
+                      <svg
+                        class="h-5 w-auto text-gray-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </li>
+                    <li class="inline-flex items-center">
+                      <a href="#" class="text-indigo-700">Dashboard</a>
+                    </li>
+                  </ul>
+ </div>               
+
+
+<br>
+
+
+<div class="grid md:grid-cols-4 grid-cols-2   gap-4">
+  <div class="bg-purple-700 text-white  p-4"><p class="text-4xl">$80</p><p>Total views</p></div>
+  
+  <div class="bg-yellow-400 p-4"><p class="text-4xl">$80</p><p>Total Earnings</p></div>
+  <div class="bg-red-600 text-white  p-4"><p class="text-4xl">$80</p><p>Referral Earnings</p></div>
+  <div class="bg-gray-900 text-white p-4"><p class="text-4xl">$80</p><p>Average CMP</p></div>
+
+</div>
+
+<div class="mt-5 annocements bg-white shadow-md">
+<div class="box-header">
+<div><i class="fa fa-bullhorn" style="font-size: 25px; margin-right:6px;"></i> 
+<h3 class="box-title">Announcements</h3>
+<br>
+<hr>
+<br>
+
+<span>
+<i class="fa fa-angle-double-right"></i> <b class="text-lg">New Withdrawal Methods:</b>
+</span>
+<p>1) PhonnePe Has added</p>
+<p>2) GooglePay Has added</p>
+<p>3) Paytm Has added</p>
+
+
+<hr/>
+
+</div>
+
+</div>
+
+
+</div>
+</div>
+
+
+
+<div class="bg-white text-center mt-10 p-3 font-bold border-b border-gray-200 rounded">
+
+
+Copyright © Bitly.help 2021 
+</div>
 </body>
 </html>
---
+++ Dashboard/navbar.php
@@ -0,0 +1,41 @@
+<nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-indigo-700 ">
+  <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+      <a class="text-lg  font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+      Bitly.help
+      </a>
+      <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" onclick="toggleNavbar('example-collapse-navbar')">
+        <i class="fa fa-bars"></i>
+      </button>
+    </div>
+    <div class="lg:flex flex-grow items-center hidden " id="example-collapse-navbar">
+      <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+        <li class="nav-item">
+          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+            <i class="fa fa-home text-lg leading-lg text-white opacity-75"></i><span class="ml-2">Home </span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+            <i class="fa fa-money text-lg leading-lg text-white opacity-75"></i><span class="ml-2">Available Balance : $1</span>
+          </a>
+          
+        </li>
+        <li class="nav-item">
+          <a class="px-3 py-2 flex items-center text-m uppercase font-bold leading-snug text-white hover:opacity-75" href="../Dashboard/sign.php">
+            <i class="fa  fa-sign-out text-lg leading-lg text-white opacity-75"></i><span class="ml-2">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<script>
+
+function toggleNavbar(collapseID){
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("flex");
+  }
+
+</script>
